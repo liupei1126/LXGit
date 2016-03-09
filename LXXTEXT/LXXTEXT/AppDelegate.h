0e6e4b9443c97c375464c508196fc209eb14d4c4
qianfeng <qianfeng@SOHO.local>
2016-03-09 17:11:38 +0800

    第一次提交

---
+++ LXXTEXT/LXXTEXT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXXTEXT
+//
+//  Created by qianfeng on 16/3/9.
+//  Copyright (c) 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXXTEXT/LXXTEXT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXXTEXT
+//
+//  Created by qianfeng on 16/3/9.
+//  Copyright (c) 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
